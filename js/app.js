fa5f84b1b5beda3978b0a4e5ccf748ffd6ffbb28
Marco Hernandez <marco7245@hotmail.com>
2023-02-21 20:43:14 -0600

    Info de Busqueda Capturada

--- js/app.js
+++ js/app.js
@@ -1,13 +1,59 @@
+//Variables 
 const resultado = document.querySelector('#resultado');
 const year = document.querySelector('#year');
+const marca = document.querySelector('#marca');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
 
 const max = new Date().getFullYear();
 const min = max - 10;
+
+//Generar un Objeto con la busqueda
+const datosBusqueda ={
+    marca : '',
+    year : '',
+    minimo : '',
+    maximo : '',
+    transmision : '',
+    color : '',
+}
 
 document.addEventListener('DOMContentLoaded',()=>{
     mostrarAutos(); //Mostrar Automoviles
 
     llenarSelect(); //Llena el Select de Años
+});
+
+//Generar un Objeto con cada Select
+marca.addEventListener('change', (e)=>{
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener('change', (e)=>{
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('change', (e)=>{
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', (e)=>{
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', (e)=>{
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', (e)=>{
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', (e)=>{
+    datosBusqueda.color = e.target.value;
 });
 
 function mostrarAutos(){
@@ -30,5 +76,4 @@ function llenarSelect(){
         opcion.textContent = i;
         year.appendChild(opcion); //Agrega las opciones de Año
     }
-
 }
